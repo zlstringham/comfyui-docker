ce86a240511a2bab3996ed720fcf1e8e53cba300
Zac Stringham <zlstringham@gmail.com>
2023-12-10 13:08:58 -0800

    Handle PYTORCH_CUDA_ALLOC_CONF as empty env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ cp -rn ../input/* input/
 cp -rn ../models/* models/
 cp -rn ../web_extensions/* web/extensions/
 
+[ -z "${PYTORCH_CUDA_ALLOC_CONF}" ] && unset PYTORCH_CUDA_ALLOC_CONF
+
 if ! [ -f "venv/bin/activate" ]; then
     echo 'Creating virtual environment...'
     python3 -m venv venv --system-site-packages
